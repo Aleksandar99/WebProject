0689f2899898ac8b829180b852bed719247d39b1
Aleksandar99 <aleksandar.keskinev@gmail.com>
2021-08-15 11:36:54 +0300

    WebProject

---
+++ WebProject/WebProject/ControllersNU/CNU.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebProject.Models;
+
+namespace WebProject.ControllersNU
+{
+
+    public class CNU
+    {
+        public static bool AddIsOK(Worker worker)
+        {
+            WebProject.Data.WebProjectContext webProjectContext = new Data.WebProjectContext(new Microsoft.EntityFrameworkCore.DbContextOptions<Data.WebProjectContext>());
+
+            int count = webProjectContext.Worker.Count();
+
+            webProjectContext.Add(worker);
+
+            return Assert.Equals(count + 1, webProjectContext.Worker.Count());
+        }
+
+        public static bool RemoveIsOK(Worker worker)
+        {
+            WebProject.Data.WebProjectContext webProjectContext = new Data.WebProjectContext(new Microsoft.EntityFrameworkCore.DbContextOptions<Data.WebProjectContext>());
+
+            int count = webProjectContext.Worker.Count();
+
+            webProjectContext.Remove(worker);
+
+            return Assert.Equals(count - 1, webProjectContext.Worker.Count());
+        }
+
+        public static bool UpdateIsOK(Worker worker)
+        {
+            WebProject.Data.WebProjectContext webProjectContext = new Data.WebProjectContext(new Microsoft.EntityFrameworkCore.DbContextOptions<Data.WebProjectContext>());
+
+            string newWorkerName = worker.FirstName + worker.FirstName + worker.FirstName;
+
+            worker.FirstName = newWorkerName;
+
+            webProjectContext.Update(worker);
+
+            return Assert.Equals(worker.FirstName, newWorkerName);
+        }
+
+        public static void DetailsIsOK(int workerId)
+        {
+            WebProject.Data.WebProjectContext webProjectContext = new Data.WebProjectContext(new Microsoft.EntityFrameworkCore.DbContextOptions<Data.WebProjectContext>());
+
+            Worker worker = webProjectContext.Worker.FirstOrDefault(a => a.Id == workerId);
+
+            Assert.IsNotNull(worker, "Null on id = " + workerId);
+        }
+
+        public static void SetIdIsOK(int workerId, int newWorkerId)
+        {
+            WebProject.Data.WebProjectContext webProjectContext = new Data.WebProjectContext(new Microsoft.EntityFrameworkCore.DbContextOptions<Data.WebProjectContext>());
+
+            Worker worker = webProjectContext.Worker.FirstOrDefault(a => a.Id == workerId);
+
+            worker.Id = newWorkerId;
+
+            try
+            {
+                webProjectContext.Update(worker);
+
+                Assert.Fail();
+
+            }
+            catch (Exception)
+            {
+                Assert.Pass("Id is not set", "id = " + workerId);
+            }
+
+        }
+
+
+    }
+}
---
+++ WebProject/WebProject/Models/Worker.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProject.Models
+{
+    public enum WorkerStatus
+    {
+        Unsigned,
+        Working,
+        Fired,
+        TemprorayNotWorking
+    }
+
+    public enum EducationType
+    {
+        Unsigned,
+        HighSchool,
+        UniversityBachalor,
+        UniversityMaster,
+        UniversityPhd,
+        UniversityHigherThanPhd
+    }
+
+    public class Worker
+    {
+        //[Key()]
+        public int Id { get; set; }
+
+        [DisplayName("First Name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First name can not be empty"), MinLength(length: 3, ErrorMessage = "First name must be at least 3 symbols"), MaxLength(length: 20, ErrorMessage = "First name must be at most 20 symbols")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last name can not be empty"), MinLength(length: 3, ErrorMessage = "Last name must be at least 3 symbols"), MaxLength(length: 20, ErrorMessage = "Last name must be at most 20 symbols")]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Full Name")]
+        public string FullName { get => string.Concat(FirstName, " ", LastName); }
+
+        [DataType(DataType.Date, ErrorMessage = "Birth date can not be empty")]
+        [DisplayName("Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Position can not be empty"), MinLength(length: 3, ErrorMessage = "Position must be at least 3 symbols"), MaxLength(length: 20, ErrorMessage = "Position must be at most 20 symbols")]
+        [DisplayName("Position")]
+        public string Position { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Hire date can not be empty")]
+        [DisplayName("Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Fire Date")]
+        public DateTime? FireDate { get; set; }
+
+        [Required]
+        [DisplayName("Worker Status")]
+        public WorkerStatus WorkerStatus { get; set; }
+
+        [Required]
+        [DisplayName("Education Type")]
+        public EducationType EducationType { get; set; }
+
+        public Worker()
+        {
+
+        }
+    }
+}
